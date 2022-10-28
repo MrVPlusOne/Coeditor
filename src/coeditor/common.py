acbdc67064e71032c5a25c49ceffc80fe90a751b
Jiayi Wei <wjydzh1@gmail.com>
2022-10-28 13:38:05 -0500

    Properly handle merge commits.

--- src/coeditor/common.py
+++ src/coeditor/common.py
@@ -4,6 +4,16 @@ from typing import *
 import libcst as cst
 from textwrap import dedent
 from pathlib import Path
+from tqdm import tqdm
+import subprocess
 
 T1 = TypeVar("T1")
 T2 = TypeVar("T2")
+
+
+def run_command(args: Sequence[str], cwd: str | Path) -> str:
+    return subprocess.check_output(
+        args,
+        cwd=cwd,
+        text=True,
+    )
--- src/coeditor/history.py
+++ src/coeditor/history.py
@@ -1,6 +1,5 @@
 # utils for computing editing history from git commits
 
-import subprocess
 from spot.static_analysis import (
     ElemPath,
     ModuleName,
@@ -86,10 +85,15 @@ class ProjectEdit:
     before: PythonProject
     after: PythonProject
     changes: dict[ModuleName, ModuleEdit]
-    description: str
+    commit_info: "CommitInfo | None"
 
     def __repr__(self):
-        return f"ProjectEdit(msg={repr(self.description)}, changed={list(self.changes.keys())})"
+        if self.commit_info is None:
+            return f"ProjectEdit(changed={self.changes})"
+        else:
+            return (
+                f"ProjectEdit(msg={repr(self.commit_info.msg)}, changed={self.changes})"
+            )
 
     @staticmethod
     def from_code_changes(
@@ -97,7 +101,7 @@ class ProjectEdit:
         code_changes: Mapping[ModuleName, str | None],
         symlinks: Mapping[ModuleName, ModuleName] = {},
         src2module: Callable[[str], cst.Module | None] = parse_cst_module,
-        description: str = "",
+        commit_info: "CommitInfo | None" = None,
     ) -> "ProjectEdit":
         modules = before.modules
         changes = dict[ModuleName, ModuleEdit]()
@@ -127,25 +131,24 @@ class ProjectEdit:
             # module2src_file is kept emtpy for now
             PythonProject(before.root_dir, modules, dict(symlinks), dict()),
             changes,
-            description,
+            commit_info,
         )
 
 
 def file_content_from_commit(
     project_dir: Path,
-    commit: int,
+    commit: str,
     path: str,
 ) -> str:
-    return subprocess.check_output(
-        ["git", "show", f"HEAD~{commit}:{path}"],
+    return run_command(
+        ["git", "show", f"{commit}:{path}"],
         cwd=project_dir,
-        text=True,
     )
 
 
 def project_from_commit(
     project_dir: Path,
-    commit: int,
+    commit: str,
     discard_bad_files: bool = True,
     file_filter: Callable[[Path], bool] = lambda p: True,
     ignore_dirs: set[str] = PythonProject.DefaultIgnoreDirs,
@@ -161,10 +164,9 @@ def project_from_commit(
         The project at the given commit.
     """
     # get the list of files in the commit
-    files_text = subprocess.check_output(
-        ["git", "ls-tree", "-r", f"HEAD~{commit}", "--name-only"],
+    files_text = run_command(
+        ["git", "ls-tree", "-r", commit, "--name-only"],
         cwd=project_dir,
-        text=True,
     )
 
     modules = dict()
@@ -215,45 +217,82 @@ def _path_to_mod_name(path: str) -> ModuleName:
     return PythonProject.rel_path_to_module_name(Path(path))
 
 
-def edits_from_git_history(
+@dataclass
+class CommitInfo:
+    hash: str
+    parents: tuple[str, ...]
+    msg: str
+
+
+def get_commit_history(
     project_dir: Path,
-    start_commit: int,
-    end_commit: int = 0,
+    max_hisotry: int,
+    commit_id: str = "HEAD",
+) -> list[CommitInfo]:
+    """Get the commit history of the project, start from the given `commit_id`,
+    going backward in time.
+    When a merge commit is encountered, the second parent (the branch that's
+    being merged in) is used as the history.
+    """
+    commit_id = run_command(
+        ["git", "rev-parse", commit_id],
+        cwd=project_dir,
+    ).strip()
+    history = []
+    for _ in range(max_hisotry):
+        lines = run_command(
+            ["git", "cat-file", "-p", commit_id],
+            cwd=project_dir,
+        ).splitlines()
+        parents = []
+        for line in lines[1:]:
+            if line.startswith("parent "):
+                parents.append(line.split(" ")[1])
+            else:
+                break
+        commit_msg = run_command(
+            ["git", "show", commit_id, "-s", "--format=%s"],
+            cwd=project_dir,
+        ).strip()
+        history.append(CommitInfo(commit_id, tuple(parents), commit_msg))
+        if not parents:
+            break
+        commit_id = parents[-1]
+    return history
+
+
+def edits_from_commit_history(
+    project_dir: Path,
+    history: Sequence[CommitInfo],
     src2module: Callable[[str], cst.Module | None] = parse_cst_module,
     ignore_dirs=PythonProject.DefaultIgnoreDirs,
+    verbose=True,
 ) -> list[ProjectEdit]:
-    """Compute the edits to a project from git history.
-
-    Args:
-        project_dir: The directory of the project.
-        start_commit: The start time step relative to HEAD.
-        end_commit: The end time step relative to HEAD.
+    """Incrementally compute the edits to a project from the git history.
 
     Returns:
         A list of edits to the project, from past to present.
     """
-    assert end_commit >= 0
-    assert start_commit >= end_commit
 
     def is_src(path_s: str) -> bool:
         path = Path(path_s)
         return path.suffix == ".py" and all(p not in ignore_dirs for p in path.parts)
 
-    project = project_from_commit(project_dir, start_commit, src2module=src2module)
+    commit_now = history[-1]
+    if verbose:
+        print(f"Retriving initial project from commit {commit_now.hash}")
+    project = project_from_commit(project_dir, commit_now.hash, src2module=src2module)
     edits = []
 
-    for t in range(start_commit - 1, end_commit - 1, -1):
-        # get commit message
-        commit_msg = subprocess.check_output(
-            ["git", "show", f"HEAD~{t}", "-s", "--format=%s"],
-            cwd=project_dir,
-            text=True,
-        )
+    for commit_next in tqdm(
+        list(reversed(history[:-1])),
+        desc="Retriving commits",
+        disable=not verbose,
+    ):
         # get changed files
-        changes_text = subprocess.check_output(
-            ["git", "diff", f"HEAD~{t+1}", f"HEAD~{t}", "--name-status"],
+        changes_text = run_command(
+            ["git", "diff", commit_now.hash, commit_next.hash, "--name-status"],
             cwd=project_dir,
-            text=True,
         )
 
         code_changes = dict[ModuleName, str | None]()
@@ -264,20 +303,23 @@ def edits_from_git_history(
                     code_changes[_path_to_mod_name(path)] = None
                 case ["M" | "A", path] if is_src(path):
                     code_changes[_path_to_mod_name(path)] = file_content_from_commit(
-                        project_dir, t, path
+                        project_dir, commit_next.hash, path
                     )
                 case [tag, path1, path2] if (
                     tag.startswith("R") and is_src(path1) and is_src(path2)
                 ):
                     code_changes[_path_to_mod_name(path2)] = file_content_from_commit(
-                        project_dir, t, path2
+                        project_dir, commit_next.hash, path2
                     )
                     code_changes[_path_to_mod_name(path1)] = None
 
         edit = ProjectEdit.from_code_changes(
-            project, code_changes, description=commit_msg
+            project,
+            code_changes,
+            commit_info=commit_next,
         )
         edits.append(edit)
         project = edit.after
+        commit_now = commit_next
 
     return edits
